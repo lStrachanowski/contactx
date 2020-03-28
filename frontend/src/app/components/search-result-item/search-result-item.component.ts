f2d149bbd9570d1e736a70f9f27063e478eb6d2d
Strachu <lukasz.strachanowski@gmail.com>
2020-03-28 17:34:16 +0100

    Contacts service created and injected to serach results items

--- frontend/src/app/components/search-result-item/search-result-item.component.ts
+++ frontend/src/app/components/search-result-item/search-result-item.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ContactsService} from '../../services/contacts.service';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-search-result-item',
   templateUrl: './search-result-item.component.html',
   styleUrls: ['./search-result-item.component.css']
 })
 export class SearchResultItemComponent implements OnInit {
-
-  constructor() { }
+  contactList = [];
+  constructor(private contact: ContactsService) {
+    this.contactList = contact.getContact();
+  }
 
   ngOnInit() {
+    console.log(this.contactList);
   }
 
 }
---
+++ frontend/src/app/services/contacts.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  contacts = [
+    {
+      vorname: 'Lukasz',
+      name: 'Strach',
+      contact_id: 1,
+      user_id: 'abc1',
+      company: 'Pol Transport and Forwarding sp. z o.o. sp. k',
+      address: 'ul. Bla bla 4 , 40-400 Katowice',
+      email: 'a@a.pl',
+      phone: 323523254,
+      mobile: 609900999995,
+      fax: 222222222,
+      other: 'Senior Executive at this company',
+      group: 'work'
+   },
+   {
+    vorname: 'Albert',
+    name: 'Sosnowski',
+    contact_id: 2,
+    user_id: 'abc1',
+    company: 'SPS ps.z o.o.',
+    address: 'ul. Tysiąclecia 20 , 09-500 Warszawa',
+    email: 'bbbbbbbbb@a.pl',
+    phone: 520002553,
+    mobile: 6060619191919,
+    fax: 26126115615,
+    other: null,
+    group: 'work'
+ },
+ {
+  vorname: 'Olga',
+  name: 'Borys',
+  contact_id: 3,
+  user_id: 'abc1',
+  company: null,
+  address: 'Jesionowa 9/3a , 40-500 Zabrze',
+  email: 'olga@wp.pl',
+  phone: null,
+  mobile: 6060619191919,
+  fax: null,
+  other: null,
+  group: 'private'
+}
+
+  ];
+  constructor() { }
+  getContact(){
+    return this.contacts;
+  }
+}
