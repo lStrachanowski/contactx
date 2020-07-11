75d29f1cfe760cc992b6cf02f95c28a77a87ddcb
Strachu <lukasz.strachanowski@gmail.com>
2020-07-11 09:30:32 +0200

    Settings tabs toggle added

--- frontend/src/app/components/settings-options/settings-options.component.ts
+++ frontend/src/app/components/settings-options/settings-options.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsOptionsComponent implements OnInit {
 
+  email = false;
+  pass = false;
+  nick = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  openTab(value: string) {
+    if (value === 'email') {
+      this.email = this.email ? false : true;
+    }
+    if (value === 'pass') {
+      this.pass = this.pass ? false : true;
+    }
+    if (value === 'nick') {
+      this.nick = this.nick ? false : true;
+    }
+
+  }
 }
