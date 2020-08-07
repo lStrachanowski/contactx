29d99892952fa397e91c91d9971d3216bbd6adca
Strachu <lukasz.strachanowski@gmail.com>
2020-08-07 23:25:23 +0200

    Routes refactored, only login route left

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,16 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def main_route():
-    return 'main_route'
-
-@app.route('/dashboard')
-def dashboard():
-    return 'dashboard'
-
-@app.route('/user/<id>/details')
-def user_details(id):
-    return id
-
-@app.route('user/<id>/details/notes')
-def user_notes():
-    return id
-
-@app.route('/add')
-def add_user():
-    return 'add'
-
-@app.route('/edit/<id>')
-def edit_user(id):
-    return 'edit'
-
-@app.route('/settings')
-def settings():
-    return 'settings'
-
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'GET':
+        username = request.args.get('username')
+        password = request.args.get('password')
+        print(username, password)
+    else:
+        print('post')
+    return "test"
 
 
