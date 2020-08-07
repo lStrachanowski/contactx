8127aef4fe5a43ff1480c6c643d13f476dcc510f
Strachu <lukasz.strachanowski@gmail.com>
2020-08-07 20:45:13 +0200

    Flask routes and modules created

--- backend/main.py
+++ backend/main.py
@@ -2,5 +2,32 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main_route():
+    return 'main_route'
+
+@app.route('/dashboard')
+def dashboard():
+    return 'dashboard'
+
+@app.route('/user/<id>/details')
+def user_details(id):
+    return id
+
+@app.route('user/<id>/details/notes')
+def user_notes():
+    return id
+
+@app.route('/add')
+def add_user():
+    return 'add'
+
+@app.route('/edit/<id>')
+def edit_user(id):
+    return 'edit'
+
+@app.route('/settings')
+def settings():
+    return 'settings'
+
+
+
